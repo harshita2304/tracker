3e67a49343429c76365567a6fc9aa8f21ccbb85e
Harshita <harshitas234btechcse@kccitm.edu.in>
2024-09-09 14:39:28 +0530

    adding checkboxes

---
+++ script.js
@@ -0,0 +1,14 @@
+// // Script to mark tasks as complete
+// document.addEventListener("DOMContentLoaded", () => {
+//   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+//   checkboxes.forEach((checkbox) => {
+//     checkbox.addEventListener("change", () => {
+//       if (checkbox.checked) {
+//         checkbox.nextElementSibling.classList.add("completed");
+//       } else {
+//         checkbox.nextElementSibling.classList.remove("completed");
+//       }
+//     });
+//   });
+// });
